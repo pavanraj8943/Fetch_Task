dfc998590713609593d294f5f03a923e7730751d
pavanraj8943 <pavan.64j@gmail.com>
2025-11-07 15:39:53 +0530

    pending

--- add.js
+++ add.js
@@ -3,6 +3,8 @@ const id = urlParams.get("id");
   fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then(response => response.json())
     .then(data => {
+        document.getElementById("post-id").textContent = data.id;
+      document.getElementById("user-id").textContent = data.userId;
       document.getElementById("title").textContent = data.title;
       document.getElementById("body").textContent = data.body;
     })
